535eb802ec82f9b873dad9fc996c91a2d6d228da
James Harkins <jh.harkins@protonmail.com>
2022-05-16 17:37:21 -0600

    Feat: pass request tests for Merchants#index

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,11 +8,11 @@ describe "Merchants API" do
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body)
+    merchants = JSON.parse(response.body, symbolize_names: true)
 
     expect(merchants.count).to eq(5)
 
-    mercahnts.each do |merchant|
+    merchants.each do |merchant|
       expect(merchant).to have_key(:id)
       expect(merchant[:id]).to be_an(Integer)
 
