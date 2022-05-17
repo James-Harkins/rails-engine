8ebb2dd9d4d962309297452c102ba0362fd97704
James Harkins <jh.harkins@protonmail.com>
2022-05-16 18:09:00 -0600

    Test: create request tests for Merchant::Items#index request

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -42,6 +42,27 @@ describe "Merchants API" do
 
     create_list(:item, 5, merchant_id: merchant.id)
 
-    # binding.pry
+    get "/api/v1/merchants/#{merchant.id}/items"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(Integer)
+
+      expect(item).to have_key(:name)
+      expect(item[:name]).to be_an(String)
+
+      expect(item).to have_key(:description)
+      expect(item[:description]).to be_an(String)
+
+      expect(item).to have_key(:unit_price)
+      expect(item[:unit_price]).to be_an(Float)
+
+      expect(item).to have_key(:merchant_id)
+      expect(item[:merchant_id]).to eq(merchant.id)
+    end
   end
 end
