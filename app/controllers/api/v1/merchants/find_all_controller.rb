cb77d16cda4e15558f655e26125d6a2f82e5d5b4
James Harkins <jh.harkins@protonmail.com>
2022-05-18 10:55:03 -0600

    Refactor: refactor Merchants::FindAll#index to use Merchant#find_all_by_name helper class method

--- app/controllers/api/v1/merchants/find_all_controller.rb
+++ app/controllers/api/v1/merchants/find_all_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Merchants::FindAllController < ApplicationController
   def index
-    merchants = Merchant.where("merchants.name LIKE ?", "%#{params[:name]}%")
+    merchants = Merchant.find_all_by_name(params[:name])
     render json: MerchantSerializer.new(merchants)
   end
 end
