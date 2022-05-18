e75e5922a9287dc65f9959ad8a2ff03f244fe686
James Harkins <jh.harkins@protonmail.com>
2022-05-18 10:43:18 -0600

    Feat: pass model test for Item#find_by_name class method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  def self.find_by_name(search)
+    where("LOWER(items.name) LIKE ?", "%#{search}%")
+      .order(:name)
+      .first
+  end
 end
