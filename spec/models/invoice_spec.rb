b5474af8d70a91a9545c850247839d51b0f231de
James Harkins <jh.harkins@protonmail.com>
2022-05-18 16:11:47 -0600

    Test: create model tests for Invoice and InvoiceItem relationships

---
+++ spec/models/invoice_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+describe Invoice, type: :model do
+  describe "relationships" do
+    it { should have_many :invoice_items }
+    it { should have_many(:items).through(:invoice_items) }
+  end
