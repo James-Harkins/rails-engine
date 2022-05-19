c65af9e4ffb8cb3f1f4810857bd8a509eda172aa
James Harkins <jh.harkins@protonmail.com>
2022-05-19 14:23:54 -0600

    Feat: pass Postman tests for min_price and max_price less than 0

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Items::FindController < ApplicationController
   def index
     if (params[:name] && params[:min_price]) || (params[:name] && params[:max_price])
-      render "results", status: 400
+      render status: 400
     elsif params[:name]
       item = Item.find_by_name(params[:name])
       render json: ItemSerializer.new(item)
@@ -14,8 +14,8 @@ class Api::V1::Items::FindController < ApplicationController
     elsif params[:max_price].to_i > 0
       item = Item.find_by_max_price(params[:max_price])
       render json: ItemSerializer.new(item)
-    else
-      render "results", status: 400
+    elsif params[:min_price].to_i < 0 || params[:max_price].to_i < 0
+      render json: {error: nil}, status: 400
     end
   end
 end
