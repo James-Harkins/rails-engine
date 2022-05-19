cda27c2d95742db01d142a0f79f724f48c3ca749
James Harkins <jh.harkins@protonmail.com>
2022-05-18 19:25:47 -0600

    Test: create model tests for invoice.invoice_items_only_contain_item instance method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,5 +3,8 @@ class Invoice < ApplicationRecord
   has_many :items, through: :invoice_items
 
   def self.only_containing_item(item_id)
+    all.find_all do |invoice|
+      invoice.invoice_items.length == 1
+    end
   end
 end
